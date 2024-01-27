e42995912de08436fc242907626a5996b8883fad
Zayyan <qureshizayyan49@gmail.com>
2024-01-28 00:54:26 +0530

    make some changes

--- script.js
+++ script.js
@@ -1,53 +1,89 @@
 
 let password = document.querySelector('#password');
 let btn = document.querySelector('.btn');
+let error = document.querySelector('.error');
+let emptyfield = document.querySelector('.empty-field');
+error.style.display = 'none';
+emptyfield.style.display = 'none';
 
-password.addEventListener('Keyup', (e) => {
+const key = () => {
+    password.addEventListener('Keyup', (e) => {
 
-    if (e.key === 'Enter') {
-        if (password.value.length < 8) {
-            alert('characters must be length of 8');
+        if (e.key === 'Enter') {
+
+            if (password.value.length < 8) {
+                alert('characters must be length of 8');
+            }
+
+            if (!/[A-Z]/.test(password.value)) {
+                alert('please enter one upperCase ');
+            }
+
+            if (!/\d/.test(password.value)) {
+                alert('please enter at least one digit');
+            }
+
+            if (!/[a-z]/.test(password.value)) {
+                alert('please enter a one LowerCase Letter');
+            }
+        }
+    })
+}
+
+const click = () => {
+    btn.addEventListener('click', () => {
+
+        emptyfield.style.display = 'none';
+        error.style.display = 'none';
+
+        if (password.value === '') {
+            emptyfield.style.display = 'block';
         }
 
-        if (!/[A-Z]/.test(password.value)) {
-            alert('please enter one upperCase ');
+        if (password.value !== '') {
+
+            if (password.value.length < 8) {
+                error.style.display = 'block';
+            }
+
+            if (!/[A-Z]/.test(password.value)) {
+                error.style.display = 'block';
+            }
+
+            if (!/\d/.test(password.value)) {
+                error.style.display = 'block';
+            }
+
+            if (!/[a-z]/.test(password.value)) {
+                error.style.display = 'block';
+            }
         }
+    })
+}
 
-        if (!/\d/.test(password.value)) {
-            alert('please enter at least one digit');
-        }
+click()
 
-        if (!/[a-z]/.test(password.value)) {
-            alert('please enter a one LowerCase Letter');
-        }
-    }
+// const click = () => {
+//     btn.addEventListener('click', () => {
+//         emptyfield.style.display = 'none';
+//         error.style.display = 'none';
 
-})
+//         if (password.value === '') {
+//             emptyfield.style.display = 'block';
+//         } else if (password.value.length < 8) {
+//             error.textContent = "Password must be at least 8 characters long.";
+//             error.style.display = 'block';
+//         } else if (!/[A-Z]/.test(password.value)) {
+//             error.textContent = "Password must contain at least one uppercase letter.";
+//             error.style.display = 'block';
+//         } else if (!/\d/.test(password.value)) {
+//             error.textContent = "Password must contain at least one digit.";
+//             error.style.display = 'block';
+//         } else if (!/[a-z]/.test(password.value)) {
+//             error.textContent = "Password must contain at least one lowercase letter.";
+//             error.style.display = 'block';
+//         }
+//     });
+// };
 
-btn.addEventListener('click', () => {
-
-    if (password.value === '') {
-        console.log('Enter a valid password');
-    }
-
-    if (password.value !== '') {
-
-        if (password.value.length < 8) {
-            alert('characters must be length of 8');
-        }
-
-        if (!/[A-Z]/.test(password.value)) {
-            alert('please enter one upperCase ');
-        }
-
-        if (!/\d/.test(password.value)) {
-            alert('please enter at least one digit');
-        }
-
-        if (!/[a-z]/.test(password.value)) {
-            alert('please enter a one LowerCase Letter');
-        }
-    }
-
-})
-
+// click();
